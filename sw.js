34a43e897811bbf4fd87295a9c67915aa8f655b9
borami4567-sketch <borami4567@gmail.com>
2026-04-09 04:09:50 +0900

    Update service worker for caching strategy and files

--- sw.js
+++ sw.js
@@ -8,7 +8,35 @@ self.addEventListener('install', e => {
 
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))const CACHE = 'sesco-care-v2';
+const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+// 네트워크 우선  실패 시 캐시 사용 (항상 최신 버전 유지)
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        // 네트워크 성공 시 캐시도 업데이트
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
+
   ));
   self.clients.claim();
 });
