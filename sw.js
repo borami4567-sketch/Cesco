73d9b61f43cbb4d5351d38b89c73f183aca18547
borami4567-sketch <borami4567@gmail.com>
2026-04-18 21:15:38 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,4 @@
-﻿const CACHE = 'sesco-care-v4';
-const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-// 네트워크 우선  실패 시 캐시 사용 (항상 최신 버전 유지)
+// Service Worker - 캐싱 비활성화 (항상 최신 버전 제공)
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        // 네트워크 성공 시 캐시도 업데이트
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
